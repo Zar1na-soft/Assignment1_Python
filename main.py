6b9e542c69a81c07c135ae9cd4d9a26a88bd5791
Zar1na-soft <80224057+Zar1na-soft@users.noreply.github.com>
2021-09-22 15:05:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
-report = cg.get_coins_markets(vs_currency='usd')
-print(report)
+answer = cg.get_coins_markets(vs_currency='usd')
+print(answer)
 
 num = int(input("Put number: "))
 sumnum = 0
 while sumnum < num:
     sumnum += 1
-    print(sumnum,"-", report[sumnum]['name'], report[sumnum]['market_cap'])
+    print(sumnum,"-", answer[sumnum]['name'], answer[sumnum]['market_cap'])
 
